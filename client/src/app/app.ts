b019bc9828a04c785c764d977e1cc7714e9f2af8
Abdul Manan <138243512+abdulmanan093@users.noreply.github.com>
2025-08-11 19:42:42 +0500

    End of section 6

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,43 +1,42 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
+import { Component, inject } from '@angular/core';
 import { Nav } from '../layout/nav/nav';
-import { Home } from '../features/home/home';
-import { AccountService } from '../core/services/account-service';
-import { User } from '../types/user';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App implements OnInit {
-  private accountService = inject(AccountService);
-  private http = inject(HttpClient);
-  protected title = 'SociaLink';
-  protected users = signal<User[]>([]);
-
-  async ngOnInit() {
-    this.users.set(await this.getusers());
-    this.setCurrentUser();
-  }
-
-  setCurrentUser() {
-    const userString = localStorage.getItem('user');
-    if (!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-
-  async getusers() {
-    try {
-      return lastValueFrom(
-        this.http.get<User[]>('http://localhost:5182/api/users')
-      );
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-  }
+export class App {
+  protected router = inject(Router);
 }
+// export class App implements OnInit {
+//   private accountService = inject(AccountService);
+//   private http = inject(HttpClient);
+//   protected title = 'SociaLink';
+//   protected users = signal<User[]>([]);
+
+//   async ngOnInit() {
+//     this.users.set(await this.getusers());
+//     this.setCurrentUser();
+//   }
+
+//   setCurrentUser() {
+//     const userString = localStorage.getItem('user');
+//     if (!userString) return;
+//     const user = JSON.parse(userString);
+//     this.accountService.currentUser.set(user);
+//   }
+
+//   async getusers() {
+//     try {
+//       return lastValueFrom(
+//         this.http.get<User[]>('http://localhost:5182/api/users')
+//       );
+//     } catch (error) {
+//       console.log(error);
+//       throw error;
+//     }
+//   }
+// }
