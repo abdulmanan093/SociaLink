791749863379c792c14c645609eece8076cc54e8
Abdul Manan <138243512+abdulmanan093@users.noreply.github.com>
2025-08-25 23:55:50 +0500

    End of section 15

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -11,10 +11,20 @@ public class DataContext(DbContextOptions<DataContext> options) : DbContext(opti
     public DbSet<Member> Members { get; set; }
     public DbSet<Photo> Photos { get; set; }
     public DbSet<MemberLike> Likes { get; set; }
+    public DbSet<Message> Messages { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Message>()
+            .HasOne(x => x.Recipient)
+            .WithMany(m => m.MessagesReceived)
+            .OnDelete(DeleteBehavior.Restrict);
+        modelBuilder.Entity<Message>()
+            .HasOne(x => x.Sender)
+            .WithMany(m => m.MessagesSent)
+            .OnDelete(DeleteBehavior.Restrict);
 
         modelBuilder.Entity<MemberLike>().HasKey(x => new { x.SourceMemberId, x.TargetMemberId });
 
@@ -35,6 +45,10 @@ public class DataContext(DbContextOptions<DataContext> options) : DbContext(opti
             v => DateTime.SpecifyKind(v, DateTimeKind.Utc)
         );
 
+        var nullableDateTimeConverter = new ValueConverter<DateTime?, DateTime?>(
+            v => v.HasValue ? v.Value.ToUniversalTime() : null,
+            v => v.HasValue ? DateTime.SpecifyKind(v.Value, DateTimeKind.Utc) : null
+        );
         foreach (var entityType in modelBuilder.Model.GetEntityTypes())
         {
             foreach (var property in entityType.GetProperties())
@@ -42,6 +56,10 @@ public class DataContext(DbContextOptions<DataContext> options) : DbContext(opti
                 if (property.ClrType == typeof(DateTime))
                 {
                     property.SetValueConverter(dateTimeConverter);
+                }
+                else if (property.ClrType == typeof(DateTime?))
+                {
+                    property.SetValueConverter(nullableDateTimeConverter);
                 }
             }
         }
