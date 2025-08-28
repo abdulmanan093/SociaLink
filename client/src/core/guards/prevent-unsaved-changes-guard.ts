13a190da2e22f1a8e07ce1d899edf00ebddadcdd
Abdul Manan <138243512+abdulmanan093@users.noreply.github.com>
2025-08-28 23:07:26 +0500

    section 18 fix bugs

--- client/src/core/guards/prevent-unsaved-changes-guard.ts
+++ client/src/core/guards/prevent-unsaved-changes-guard.ts
@@ -1,14 +1,16 @@
 import { CanDeactivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { ConfirmDialogService } from '../services/confirm-dialog-service';
 import { MemberProfile } from '../../features/members/member-profile/member-profile';
 
 export const preventUnsavedChangesGuard: CanDeactivateFn<MemberProfile> = (
   component
 ) => {
   if (component.editForm?.dirty) {
-    return confirm(
+    const confirmDialog = inject(ConfirmDialogService);
+    return confirmDialog.confirm(
       'Are you sure you want to continue? All unsaved changes will be lost'
     );
   }
-
   return true;
 };
